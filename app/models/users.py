f3e41a930f03f83fd579dccef9e742b2999f0409
SungminSo <soos196@hanyang.ac.kr>
2020-10-31 19:36:08 +0900

    fix User model field property

--- app/models/users.py
+++ app/models/users.py
@@ -13,7 +13,7 @@ class User(db.Model):
     __table_name__ = "user"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    uuid = db.Column(db.String(128), unique=True, nullable=True)
+    uuid = db.Column(db.String(128), unique=True, nullable=False)
     name = db.Column(db.String(32), nullable=False)
     email = db.Column(db.String(128), unique=True, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False)
