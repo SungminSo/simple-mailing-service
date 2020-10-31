d3a330e87b204e064168a64ee261168b785a9a0d
SungminSo <soos196@hanyang.ac.kr>
2020-10-31 20:28:19 +0900

    creeate user_api send_mail_to_all_users temporarily

--- app/views/users.py
+++ app/views/users.py
@@ -6,6 +6,11 @@ from ..models import db
 from ..models.users import User
 from ..utils.validate import validate_email
 
+import requests
+
+# select 422 instead of 400 on validate failure
+# notes: https://stackoverflow.com/questions/16133923/400-vs-422-response-to-post-of-data
+
 user_api = Blueprint('user', __name__)
 
 
@@ -37,8 +42,6 @@ def subscribe_service():
         return json_response({'errorMsg': 'please check your request data'}, 400)
 
     try:
-        # select 422 instead of 400 on validate failure
-        # notes: https://stackoverflow.com/questions/16133923/400-vs-422-response-to-post-of-data
         if len(name) == 0 or len(email) == 0:
             return json_response({'errorMsg': 'please check name and email'}, 422)
         if not validate_email(email):
@@ -55,15 +58,18 @@ def subscribe_service():
         email=email,
     )
 
-    # create a savepoint in case of race condition
-    db.session.begin_nested()
-    try:
-        user_uuid = user.create()
-        db.session.commit()
-        return json_response({'uuid': user_uuid}, 201)
-    except IntegrityError:
-        db.session.rollback()
-        return json_response({'errorMsg': 'fail to subscribe'}, 409)
+    # # create a savepoint in case of race condition
+    # db.session.begin_nested()
+    # try:
+    #     user_uuid = user.create()
+    #     db.session.commit()
+    #     return json_response({'uuid': user_uuid}, 201)
+    # except IntegrityError:
+    #     db.session.rollback()
+    #     return json_response({'errorMsg': 'fail to subscribe'}, 409)
+    user_uuid = user.create()
+    db.session.commit()
+    return json_response({'uuid': user_uuid}, 201)
 
 
 @user_api.route('/users/unsubscribe/<string:uuid>/<string:email>', methods=['DELETE'])
@@ -82,3 +88,37 @@ def unsubscribe_service(uuid: str, email: str):
     db.session.commit()
 
     return json_response({}, 204)
+
+
+@user_api.route('/mail', methods=['POST'])
+def send_mail_to_all_users():
+    try:
+        req_data = request.get_json()
+        subject = req_data['subject']
+        content = req_data['content']
+    except TypeError:
+        return json_response({'errorMsg': 'please send request data'}, 400)
+    except KeyError:
+        return json_response({'errorMsg': 'please check your request data'}, 400)
+
+    try:
+        if len(subject) == 0 or len(content) == 0:
+            return json_response({'errorMsg': 'please check subject and content'}, 422)
+    except TypeError:
+        return json_response({'errorMsg': 'please check subject and content data type'}, 400)
+
+    users = User.get_all_users(page=1)
+
+    for user in users:
+        headers = {
+            'Authorization': 'herren-recruit-python',
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+        data = {'mailto': user.email, 'subject': subject, 'content': content}
+        res = requests.post('http://python.recruit.herrencorp.com/api/v1/mail', data=data, headers=headers)
+        if res.status_code != 201:
+            print(res.status_code)
+            print(res.content)
+            return json_response({'errorMsg': f"fail to send an email to {user.email}"}, 500)
+
+    return json_response({'status': 'success'}, 201)
