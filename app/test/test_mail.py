e85c1ff74ddd4b870996a5fccc1a6ac2aedb6906
SungminSo <soos196@hanyang.ac.kr>
2020-11-01 18:17:59 +0900

    create get_user_mails test

--- app/test/test_mail.py
+++ app/test/test_mail.py
@@ -28,3 +28,19 @@ def test_send_mail_to_all_users_422(api):
     assert error_msg == 'please check subject and content'
 
 
+def test_get_user_mails(api):
+    email = 'test@gmail.com'
+    res = api.get(f'/api/v1/mails/{email}')
+    assert res.status_code == 200
+
+    mails = res.json['mails']
+    assert type(mails) == list
+
+
+def test_get_user_mails_422(api):
+    email = 'test'
+    res = api.get(f'/api/v1/mails/{email}')
+    assert res.status_code == 422
+
+    error_msg = res.json['errorMsg']
+    assert error_msg == 'wrong format of email'
