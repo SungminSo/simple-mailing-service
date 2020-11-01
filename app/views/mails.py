322f41c705bae8ec69d2d5e57993fc9cb877d75d
SungminSo <soos196@hanyang.ac.kr>
2020-11-01 20:50:37 +0900

    create send_mail for multiprocessing but couldn't do yet

--- app/views/mails.py
+++ app/views/mails.py
@@ -10,6 +10,22 @@ import requests
 mail_api = Blueprint('mail', __name__)
 
 
+def send_mail(subject: str, content: str, users: list) -> list:
+    fail_to_send = []
+    for user in users:
+        headers = {
+            'Authorization': Config.HERRENCORP_MAIL_AUTH,
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+        data = {'mailto': user.email, 'subject': subject, 'content': content}
+        url = Config.HERRENCORP_BASE_URL + Config.HERRENCORP_SEND_MAIL_URL
+        res = requests.post(url, data=data, headers=headers)
+        if res.status_code != 201:
+            fail_to_send.append(user.email)
+
+    return fail_to_send
+
+
 @mail_api.route('/mails', methods=['POST'])
 def send_mail_to_all_users():
     try:
@@ -34,19 +50,10 @@ def send_mail_to_all_users():
         user_page = user_num // USERS_PER_PAGE
 
     fail_to_send = []
+    # TODO: for문이 아니라 병렬처리로 처리해야함
     for page in range(1, user_page + 1):
         users = User.get_all_users(page=page)
-
-        for user in users.items:
-            headers = {
-                'Authorization': Config.HERRENCORP_MAIL_AUTH,
-                'Content-Type': 'application/x-www-form-urlencoded',
-            }
-            data = {'mailto': user.email, 'subject': subject, 'content': content}
-            url = Config.HERRENCORP_BASE_URL + Config.HERRENCORP_SEND_MAIL_URL
-            res = requests.post(url, data=data, headers=headers)
-            if res.status_code != 201:
-                fail_to_send.append(user.email)
+        fail_to_send += send_mail(subject=subject, content=content, users=users.items)
 
     if len(fail_to_send) != 0:
         return json_response({'errorMsg': f"fail to send email", 'fail_list': fail_to_send}, 500)
