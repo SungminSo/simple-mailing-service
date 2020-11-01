65633ac0136c92046b89b10daa22ebdc8034598f
SungminSo <soos196@hanyang.ac.kr>
2020-11-01 21:17:17 +0900

    do multiprocessing on send_mail_to_all_users

--- app/views/mails.py
+++ app/views/mails.py
@@ -1,4 +1,5 @@
 from flask import request, Blueprint
+from multiprocessing import Process
 
 from . import json_response
 from ..models.users import User, USERS_PER_PAGE
@@ -10,8 +11,7 @@ import requests
 mail_api = Blueprint('mail', __name__)
 
 
-def send_mail(subject: str, content: str, users: list) -> list:
-    fail_to_send = []
+def send_mail(subject: str, content: str, users: list, fail_to_send: list):
     for user in users:
         headers = {
             'Authorization': Config.HERRENCORP_MAIL_AUTH,
@@ -22,8 +22,6 @@ def send_mail(subject: str, content: str, users: list) -> list:
         res = requests.post(url, data=data, headers=headers)
         if res.status_code != 201:
             fail_to_send.append(user.email)
-
-    return fail_to_send
 
 
 @mail_api.route('/mails', methods=['POST'])
@@ -50,10 +48,15 @@ def send_mail_to_all_users():
         user_page = user_num // USERS_PER_PAGE
 
     fail_to_send = []
-    # TODO: for문이 아니라 병렬처리로 처리해야함
+    procs = []
     for page in range(1, user_page + 1):
         users = User.get_all_users(page=page)
-        fail_to_send += send_mail(subject=subject, content=content, users=users.items)
+        proc = Process(target=send_mail, args=(subject, content, users.items, fail_to_send))
+        procs.append(proc)
+        proc.start()
+
+    for proc in procs:
+        proc.join()
 
     if len(fail_to_send) != 0:
         return json_response({'errorMsg': f"fail to send email", 'fail_list': fail_to_send}, 500)
